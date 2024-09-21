2353a4c8e3bbf4c63ca4132e97970320b9994445
Gabriel <gabriel.backup1998@gmail.com>
2024-09-21 14:22:22 -0300

    UPDATE: Usando data e Hora

---
+++ Data e Hora/date_time.py
@@ -0,0 +1,16 @@
+# Aprender a trabalhar com datas, horas e fusos horários em Python, dominando o módulo 'datetime'
+# para manipulações precisas
+
+# Introdução ao módulo "datetime"
+# Data e Hora
+
+# É usado para lidar com data e hora
+# Possui várias classes úteis como
+# "date", "time" e "timedelta"
+
+# Usar o nome de um módulo em um arquivo da erro, nunca use o nome de um módulo em um arquivo python
+
+import datetime
+
+d = datetime.date(2024, 9, 20)
+print(d)
